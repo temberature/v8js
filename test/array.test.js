f159be2298f0921a35686bc629b148da26aa617d
Tong <temberature@gmail.com>
2018-04-28 21:02:25 +0800

    修正sort 逻辑，增加mocha 调试。

--- test/array.test.js
+++ test/array.test.js
@@ -4,20 +4,64 @@ var expect = require('chai').expect;
 var customArray = require('../src/array');
 
 describe('Array', function() {
-  var a1 = new customArray(4, 5, 1),
-    a2 = [4, 5, 1];
+  var a2 = generateArr(100);
+  var a1 = new customArray(a2);
+  // console.log(a1, a2);
 
   describe('#sort', function() {
+    profile(function () {
+      a1.sort();
+    }, function() {
+      a2.sort();
+    }, 'sort');
+
     it('array sort default small to big', function() {
-      var source = [].slice.call(a1.sort(), 0, a1.length);
-      var target = a2.sort();
-      expect(source).to.eql(target);
+      var r1 = [].slice.call(a1.sort(), 0, a1.length);
+      var r2 = a2.sort();
+      expect(r1).to.eql(r2);
     });
   });
   describe('#indexOf', function() {
+    profile(function () {
+      a1.indexOf(500);
+    }, function() {
+      a2.indexOf(500);
+    }, 'indexOf');
+
     it('no exists should return -1', function() {
+      var n = 0;
+      var r1 = a1.indexOf(n);
+      var r2 = a2.indexOf(n);
+      expect(r1).to.equal(r2);
+    });
+    it('exist return index', function() {
       var n = 5;
-      expect(indexOf(a1)).to.equal(a2.indexOf());
+      var r1 = a1.indexOf(n);
+      var r2 = a2.indexOf(n);
+      expect(r1).to.equal(r2);
     });
   });
 });
+
+function generateArr(count) {
+  var arr = new Array(count);
+  for (var i = 0; i < count; i++) {
+    arr[i] = Math.floor(Math.random() * count * 2);
+  }
+  return arr;
+}
+
+function profile(f1, f2, label) {
+  var times = 10;
+  console.time(label + '1');
+  for (var i = 0; i < times; i++) {
+    f1();
+  }
+  console.timeEnd(label + '1');
+
+  console.time(label + '2');
+  for (var i = 0; i < times; i++) {
+    f2();
+  }
+  console.timeEnd(label + '2');
+}
